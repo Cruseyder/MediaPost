95da177b39d4e1f52a7884aeb7d3b71550f4ccb6
Evandro Teixeira <ewp.dev@gmail.com>
2017-10-14 21:59:42 -0300

    Implementada interface com assinatura padrao dos métodos para classes do tipo DAO.

---
+++ dev/ts/interfaces/Dao.ts
@@ -0,0 +1,14 @@
+/**
+ * Interface DAO
+ * 
+ * Todas as classes de DAO do sistema deveram implementar a interface
+ * Isso garante que todas as DAO possuam os mesmos métodos, porém como cada um será implementado
+ * dependera unicamente da DAO em questão.
+ */
+export interface Dao <T> {
+    insert(obj: T) : void;
+    update(obj: T) : void;
+    delete(id: number) : void;
+    getById(id: number) : void;
+    get() : void;
+}
