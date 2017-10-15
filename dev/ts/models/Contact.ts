f839c529964fee7ce00664be357d245b418df20e
Evandro Teixeira <ewp.dev@gmail.com>
2017-10-15 04:08:12 -0200

    Adicionado os métodos insert, update e delete. Ambos os métodos servem como intermédiarios, fazendo uma chamada para a Classe DAO, que executa a ação solicitada no servidor.

--- dev/ts/models/Contact.ts
+++ dev/ts/models/Contact.ts
@@ -7,9 +7,24 @@ export class Contact {
     constructor(
         readonly name: string,
         readonly id?: number,
-        readonly email?: string[],
-        readonly phone?: string[]
+        readonly email?: string,
+        readonly phone?: string
     ){}
+
+    insert(obj: Contact) {
+        return this._dao
+                .insert(obj);
+    }
+
+    update(obj: Contact) {
+        return this._dao
+                .update(obj);
+    }
+
+    delete(id: number) {
+        return this._dao
+                .delete(id);
+    }
 
     getById(id: number) {
         return this._dao
