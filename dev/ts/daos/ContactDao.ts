f3049eec01520ebb6f9a909c54ebe5f2c6759488
Evandro Teixeira <ewp.dev@gmail.com>
2017-10-14 23:51:10 -0300

    Adicionado os métodos get e getById. O método get faz um requisição para o endpoint 'listar', o qual retorna um array de objetos contendo todos os contatos. O método getById recebe um ID de contato, faz uma requisição para o endpoint 'detalhes' passando esse ID, e recebe como retorno um objeto com os detalhes do contato solicitado.

--- dev/ts/daos/ContactDao.ts
+++ dev/ts/daos/ContactDao.ts
@@ -15,7 +15,29 @@ export class ContactDao implements Dao<Contact> {
 
     delete(id: number) {}
 
-    getById(id: number) {}
+    getById(id: number) {
+        return new Promise( (resolve, reject) => {
+            new Request<ApiResponse<Object>>()
+                .request(`detalhes?id=${id}&token=${Token.getToken()}`)
+                .then( res => res.data )
+                .then( (contact: Contact) => resolve( new Contact( contact.name, contact.id ) ) )
+                .catch( err => {
+                    console.log(err);
+                    reject('Ocorreu um Erro durante a requisição dos dados, por favor tente novamente em poucos instantes.');
+                });
+        });
+    }
 
-    get() {}
+    get() {
+        return new Promise( (resolve, reject) => {
+            new Request<ApiResponse<Object[]>>()
+                .request(`listar?token=${Token.getToken()}`)
+                .then( res => res.data )
+                .then( (contacts: Contact[]) => resolve( contacts.map( contact => new Contact(contact.name, contact.id ) ) ) )
+                .catch( err => {
+                    console.log(err);
+                    reject('Ocorreu um Erro durante a requisição dos dados, por favor tente novamente em poucos instantes.');
+                });
+        });
+    }
 }
