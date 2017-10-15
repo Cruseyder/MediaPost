97384f1e6a06a5eab3585eeec39f3e4c88308148
Evandro Teixeira <ewp.dev@gmail.com>
2017-10-15 21:03:25 -0200

    Implementada ação de deletar, o qual mostra um modal perguntando para o usuário se ele realmente deseja deletar o contato.

--- dev/ts/controllers/ContactController.ts
+++ dev/ts/controllers/ContactController.ts
@@ -3,6 +3,7 @@ import {ContactView} from '../views/ContactView';
 import ModalView from '../views/ModalView';
 import {EditModalFragments} from '../views/EditModalFragments';
 import {ViewModalFragments} from '../views/ViewModalFragments';
+import {DeleteModalFragments} from '../views/DeleteModalFragments';
 
 export default class ContactController {
 
@@ -23,6 +24,8 @@ export default class ContactController {
         this._body.addEventListener('click', (event) => this.close(event) );
 
         this._body.addEventListener('click', (event) => this.view(event) );
+
+        this._body.addEventListener('click', (event) => this.delete(event) );
     }
 
     /**
@@ -43,6 +46,14 @@ export default class ContactController {
         }
     }
 
+    /**
+     * Delegação de evento para o botão de visualizar
+     * 
+     * Verifica se o elemento clicado foi o botão de visualizar, solicita o objeto de Contato refente ao contato que será editado
+     * Renderiza a view com os dados do contato selecionado.
+     * 
+     * @param event Evento do Dom
+     */
     view(event: Event) {
         let target = <HTMLElement>event.target;
         let targetID: number;
@@ -50,6 +61,24 @@ export default class ContactController {
         if( target && target.matches( '[data-action="view"]' ) ) {
             targetID = parseInt( target.dataset.id );
             this._contactModel.getById(targetID).then( contact => this._modelView.update( contact, new ViewModalFragments() ) );
+        }
+    }
+
+    /**
+     * Delegação de evento para o botão de Deletar
+     * 
+     * Verifica se o elemento clicado foi o botão de deletar, solicita o objeto de Contato refente ao contato que será editado
+     * Renderiza a view solicitando ao usuário uma confirmação para deletar.
+     * 
+     * @param event Evento do Dom
+     */
+    delete(event: Event) {
+        let target = <HTMLElement>event.target;
+        let targetID: number;
+
+        if( target && target.matches( '[data-action="delete"]' ) ) {
+            targetID = parseInt( target.dataset.id );
+            this._contactModel.getById(targetID).then( contact => this._modelView.update( contact, new DeleteModalFragments() ) );
         }
     }
 
