5f9037ee515298b9366c6fd21ff89edb42fa4686
Evandro Teixeira <ewp.dev@gmail.com>
2017-10-16 02:16:41 -0200

    Implementado a função close, responsavel por remover a mensagem após um periodo de tempo x.

--- dev/ts/views/MessageView.ts
+++ dev/ts/views/MessageView.ts
@@ -1,7 +1,14 @@
 import View from './View';
 
 export class MessageView extends View<string> {
+    close() {
+        setTimeout( () => {
+            this._element.firstElementChild.classList.remove('fade-in');
+            this._element.firstElementChild.classList.add('fade-out');
+            this._element.firstElementChild.addEventListener( 'animationend', () => this._element.innerHTML = '' )
+        }, 5000 );
+    }
     _render(string: string) : string {
-        return `<div class="row">${string}</div>`;
+        return `<div class="row fade-in">${string}</div>`;
     }
 }
